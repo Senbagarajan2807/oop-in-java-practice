d8b2f089b92f0826d275c7b93382559589dec6c8
Senbagarajan2807 <senbagarajan2807@gmail.com>
2026-06-16 20:42:46 +0530

    InstanceOf Operator and IsInstanceOf Method practice

---
+++ InstanceOf.java
@@ -0,0 +1,21 @@
+class Person{
+    public void display(){
+        System.out.println("This is class Person");
+    }
+}
+class Car{
+    public void display(){
+        System.out.println("This is class Car");
+    }
+}
+
+public class InstanceOf {
+    public static void main(String[] args){
+        Person per = new Person();
+        Car car= new Car();
+        System.out.println(per instanceof Person);
+        System.out.println(car instanceof Car);
+
+    }
+    
+}
---
+++ IsInstanceOf.java
@@ -0,0 +1,20 @@
+class Person{
+    public void display(){
+        System.out.println("This is class Person");
+    }
+}
+class Car{
+    public void display(){
+        System.out.println("This is class Car");
+    }
+}
+
+
+public class IsInstanceOf {
+    public static void main(String[] args){
+        Person per = new Person();
+        Car car= new Car();
+        System.out.println(Person.class.isInstance(per));
+        System.out.println(Car.class.isInstance(car));
+    }
+}
