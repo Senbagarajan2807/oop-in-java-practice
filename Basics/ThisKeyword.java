f5d6ac3b7fbdd8ceab7de630891b52098c8f81ba
Senbagarajan2807 <senbagarajan2807@gmail.com>
2026-06-17 11:46:29 +0530

    practiced the usage of this keyword

---
+++ Basics/ThisKeyword.java
@@ -0,0 +1,16 @@
+package Basics;
+public class ThisKeyword {
+       int a;
+       int b;
+       public ThisKeyword(int a ,int b){
+        this.a = a;
+        this.b = b;
+       }
+
+    public static void main(String[] args){
+        ThisKeyword obj = new ThisKeyword(5,2);
+        System.out.println("Sum: " + obj.a+obj.b);
+        System.out.println("Product: " + obj.a*obj.b);
+
+    }
+}
